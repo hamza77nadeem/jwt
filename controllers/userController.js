0e61d3633a9b4a7be71ecb554e2b8560c5a2b9ff
rehan510 <rehan51055@gmail.com>
2022-08-06 14:04:14 -0400

    resolved token issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,62 +1,69 @@
 const express = require("express");
 const app = express();
-const path = require("path")
-var bodyParser = require('body-parser')
-const mongoose = require("mongoose")
-const hbs = require('hbs');
-const router = express.Router()
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const cookieparser = require("cookie-parser")
+const path = require("path");
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const hbs = require("hbs");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser");
+let LocalStorage = require("node-localstorage").LocalStorage;
+localStorage = new LocalStorage("./scratch");
+const userModel = require("../models/userModel.js");
 
-const userModel = require('../models/userModel.js')
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+exports.createUser = (req, res) => {
+  // console.log(req.body.itemname)
+  const oldUser = userModel.findOne({ username: req.body.username });
 
-exports.createUser=(req, res)=>{
-    // console.log(req.body.itemname)
-    const oldUser =  userModel.findOne({ username: req.body.username });
-
-    if (oldUser) {
-      return res.status(409).send("User Already Exist. Please Login");
-    }
-    const rsUsers = new userModel(req.body)
-    rsUsers.save().then(() => {
-        console.log('User: ' + req.body.username + ' Saved')
-    }).catch((err)=>{
-        console.log('User Saved Error..' + err)
+  if (oldUser) {
+    return res.status(409).send("User Already Exist. Please Login");
+  }
+  const rsUsers = new userModel(req.body);
+  rsUsers
+    .save()
+    .then(() => {
+      console.log("User: " + req.body.username + " Saved");
+    })
+    .catch((err) => {
+      console.log("User Saved Error.." + err);
     });
-    res.render("home", {abc: req.body})  
-    // res.send("User Created ... ")
-    return true
-}
+  res.render("home", { abc: req.body });
+  // res.send("User Created ... ")
+  return true;
+};
 
-exports.loginUser= async (req, res)=>{    
-    // console.log(req.body.username)
-    // console.log(password)
-    var password = req.body.password
-    const findUser = await userModel.findOne({username: req.body.username})
-    // console.log(findUser.username)    
-    if (!findUser){                             
-            res.send("User ID Not Found")
+exports.loginUser = async (req, res) => {
+  // console.log(req.body.username)
+  // console.log(password)
+
+  var password = req.body.password;
+  const findUser = await userModel.findOne({ username: req.body.username });
+  // console.log(findUser.username)
+  if (!findUser) {
+    res.send("User ID Not Found");
+  } else {
+    bcrypt.compare(
+      req.body.password,
+      findUser.password,
+      function (err, result) {
+        if (!result) {
+          // console.log('User Login Failed')
+          res.send("User Login Failed");
+        } else {
+          console.log("User Login Successfully");
+
+          const vToken = findUser.generateJWTToken();
+          res.cookie("node1", vToken);
+          localStorage.setItem("user", vToken);
+
+          // console.log(vToken)
+          res.render("home", { abc: req.body });
         }
-    else {
-        bcrypt.compare(req.body.password, findUser.password, function(err, result) {
-            if (!result){
-                // console.log('User Login Failed')
-                res.send("User Login Failed")
-            }
-            else{
-                console.log('User Login Successfully')
-
-                const vToken = findUser.generateJWTToken()  
-                res.cookie("node1", vToken)
-
-                // console.log(vToken)
-                res.render("home", {abc: req.body})
-            }                
-        });
-    }        
-}
-
+      }
+    );
+  }
+};
--- routes/route.js
+++ routes/route.js
@@ -1,51 +1,60 @@
 const express = require("express");
 const app = express();
-const path = require("path")
-var bodyParser = require('body-parser')
-const mongoose = require("mongoose")
-const hbs = require('hbs');
-const router = express.Router()
-const cookieparser = require("cookie-parser")
-const jwt = require("jsonwebtoken")
+const path = require("path");
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const hbs = require("hbs");
+const router = express.Router();
+const cookieparser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+let LocalStorage = require("node-localstorage").LocalStorage;
+localStorage = new LocalStorage("./scratch");
+require("../database/db.js");
 
-require('../database/db.js')
+mongoose.pluralize(null);
 
-mongoose.pluralize(null)
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieparser());
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cookieparser())
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const itemModel = require("../models/itemModel.js");
+const vItemController = require("../controllers/itemController.js");
+const vUserController = require("../controllers/userController.js");
 
-const itemModel = require('../models/itemModel.js')
-const vItemController = require("../controllers/itemController.js")
-const vUserController = require("../controllers/userController.js")
+router.post("/", (req, res) => {
+  res.send("<h1>POST Function Called ... </h1>");
+});
 
-router.post("/", (req, res)=>{
-    res.send("<h1>POST Function Called ... </h1>")
-})
+router.get("/", (req, res) => {
+  // res.send("<h1>GET Function Called ... </h1>")
+  // localStorage.setItem('bgcolor', 'red');
+  const token = localStorage.getItem("user");
+  if (token) {
+    res.render("home");
+  } else {
+    res.render("userlogin");
+  }
+});
 
-router.get("/", (req, res)=>{
-    // res.send("<h1>GET Function Called ... </h1>")
-    res.render("userlogin")
-})
-
-router.get("/reg", (req, res)=>{
-    res.render("userRegister")
-})
-
-router.get("/login", (req, res)=>{
-    res.render("userlogin")
-})
+router.get("/reg", (req, res) => {
+  res.render("userRegister");
+});
+router.get("/home", (req, res) => {
+  res.render("home");
+});
+router.get("/login", (req, res) => {
+  res.render("userlogin");
+});
 // router.post("/api/save", urlencodedParser, (req, res)=>{
 //     console.log(req.body)
 // })
 
 // router.get("/setcookie", (req, res)=>{
-//     const token = jwt.sign("62e14bb5d1e9782229095ea7", "FULLSTACKWEBPNYTRAININGSMEANMERN")    
+//     const token = jwt.sign("62e14bb5d1e9782229095ea7", "FULLSTACKWEBPNYTRAININGSMEANMERN")
 //     console.log(token)
 //     res.cookie("jwttoken", token)
 //     // console.log(req.signedCookies)
@@ -54,14 +63,13 @@ router.get("/login", (req, res)=>{
 //     console.log(verify)
 // })
 
-router.post("/api/save", urlencodedParser, vItemController.createItem)
-router.get("/api/all", vItemController.showAllItem)
-router.get("/api/find/:id", vItemController.findItem)
-router.put("/api/update/:id", vItemController.updateItem)
-router.delete("/api/delete/:id", vItemController.deleteItem)
+router.post("/api/save", urlencodedParser, vItemController.createItem);
+router.get("/api/all", vItemController.showAllItem);
+router.get("/api/find/:id", vItemController.findItem);
+router.put("/api/update/:id", vItemController.updateItem);
+router.delete("/api/delete/:id", vItemController.deleteItem);
 
-router.post("/api/usersave", urlencodedParser, vUserController.createUser)
-router.post("/api/userlogin", urlencodedParser, vUserController.loginUser)
+router.post("/api/usersave", urlencodedParser, vUserController.createUser);
+router.post("/api/userlogin", urlencodedParser, vUserController.loginUser);
 
-
-module.exports = router
+module.exports = router;
